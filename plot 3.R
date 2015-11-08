eb1bb500e50192b6548b7ffe6f18e8e1bdee5b2b
chjacamp <chjacamp@gmail.com>
2015-11-08 13:20:01 -0700

    added plot 2, plot 3

--- plot 3.R
+++ plot 3.R
@@ -1,5 +1,3 @@
-## Plot 1 ##
-
 file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 
 library(lubridate)
@@ -11,18 +9,18 @@ library(data.table)
 
 power <- fread("household_power_consumption.txt", na.strings ="?")
 
-## set date class d-m-y with lubridate
-power$Date <- dmy(power$Date)
 
 ##I'm not 100% sure why interval behaves this way...but this is the correct way:
 interval <- new_interval("2007-01-31 00:00:00", "2007-02-02")
 
 ##subset within the interval
-power_sub <- power[power$Date %within% interval] #on a side note, within is
-                                                 #almost no different from mutate
+power_sub <- power[power$Date %within% interval]
 
-## make a histogram
-hist(power_sub$Global_active_power, col="red")
+##so what are we measuring in? hours? Oh let's just create that index then :)
+power_sub <- mutate(power_sub, index=1:2880)
 
+power$Date <- dmy_hms(paste(power$Date, power$Time))
 
-
+with(power_sub, points(Sub_metering_3 ~ index, col="blue", type = "l"))
+with(power_sub, points(Sub_metering_2 ~ index, col="red", type = "l"))
+with(power_sub, points(Sub_metering_1 ~ index), col="black", type = "l")
---
+++ plot2.R
@@ -0,0 +1,29 @@
+#plot 2 
+# essentially, global active power by hour
+
+file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+library(lubridate)
+library(data.table)
+
+# if(!dir.exists("./Data")) {dir.create("./Data")}
+# if(!file.exists("/Data/householdpower.zip")) {download.file(file_url, "./Data/householdpower.zip")}
+# unzip("./Data/householdpower.zip", exdir = "./Data")
+
+## power <- fread("household_power_consumption.txt", na.strings ="?")
+
+
+##I'm not 100% sure why interval behaves this way...but it works: 
+interval <- new_interval("2007-01-31 00:00:00", "2007-02-02")
+
+##subset within the interval
+power_sub <- power[power$Date %within% interval]
+
+##this lovely gem is from Al Warren on the class forums:
+power_sub$Date<- ymd_hms(paste(power_sub$Date, power_sub$Time))
+
+## open file cnxn, plot it, close it!
+
+png(filename="plot2.png")
+with(power_sub, plot(Global_active_power ~ Date, type="l"))
+dev.off()
